07678c336d2e2263fd1bedcd6a8bea8a0e1ad161
jkrg <98776845+jkrge@users.noreply.github.com>
2023-09-15 17:03:29 +0300

    Add files via upload

--- test.cmd
+++ test.cmd
@@ -1,22 +1,27 @@
 @echo off
 setlocal enabledelayedexpansion
-for %%f in ("backup\*.zip") do (
-	for /f "tokens=4" %%s in ('7z.exe t "%%f" ^| find "Physical Size = "') do (set size=%%s)
-	7z.exe t "%%f"
+set folderpath=C:\task\backup
+set szpath=C:\task
+set logpath=C:\logs
+if not exist C:\logs (mkdir C:\logs)
+for %%f in ("%folderpath%\*.zip") do (
+	for /f "tokens=4" %%s in ('%szpath%\7z.exe t "%%f" ^| find "Physical Size = "') do (set size=%%s)
+	%szpath%\7z.exe t "%%f"
 	echo !errorlevel!
 	if !errorlevel! equ 0 (
 		if !size! geq 10485760 (
-	echo "%date% %time% File %%f verified successfully" >> logs\info.log
+	echo "%date% %time% File %%f verified successfully" >> %logpath%\info.log
 	) else (
 	set namef=%%f
 	call :errorsize
 	)
 	) else (
-	echo "%date% %time% File %%f has an error" >> logs\error.log
+	echo "%date% %time% File %%f has an error" >> %logpath%\error.log
 	)
 	)
+	pause
 	exit
 :errorsize
-echo "%date% %time% Size of %namef% less than 10MB (%size%!B)"	>> logs\error.log
+echo "%date% %time% Size of %namef% less than 10MB (%size%!B)"	>> %logpath%\error.log
 set !errorlevel!=1
 exit /b
