858d5676965ef13a9866da1d4e549f254c9e64f7
jkrg <98776845+jkrge@users.noreply.github.com>
2023-09-15 16:50:00 +0300

    Add files via upload

--- test.cmd
+++ test.cmd
@@ -2,20 +2,21 @@
 setlocal enabledelayedexpansion
 for %%f in ("backup\*.zip") do (
 	for /f "tokens=4" %%s in ('7z.exe t "%%f" ^| find "Physical Size = "') do (set size=%%s)
-	echo Size is !size!
-	if !size! leq 10485760 (
-	set namef=%%f
-	call :errorsize)
-	7z.exe t "%%f" 
+	7z.exe t "%%f"
 	echo !errorlevel!
 	if !errorlevel! equ 0 (
+		if !size! geq 10485760 (
 	echo "%date% %time% File %%f verified successfully" >> logs\info.log
+	) else (
+	set namef=%%f
+	call :errorsize
+	)
 	) else (
 	echo "%date% %time% File %%f has an error" >> logs\error.log
 	)
 	)
-	pause
+	exit
 :errorsize
 echo "%date% %time% Size of %namef% less than 10MB (%size%!B)"	>> logs\error.log
+set !errorlevel!=1
 exit /b
-pause
